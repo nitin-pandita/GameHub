133050669efa79299da654645f1d8becef39ae24
nitin-pandita <nitin_207013@saitm.org>
2023-07-12 01:18:10 +0530

    Showing a skeleton while fetching Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,22 +1,26 @@
-import { HStack, Image, List, ListItem } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Skeleton } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenere";
 import getCroppedUrlImage from "../services/image-url";
 import { Text } from "@chakra-ui/react";
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+  if (error) return null;
+  if (isLoading) return <Skeleton height={"20px"} />;
   return (
     <List>
       {data.map((genres) => (
-        <ListItem key={genres.id} paddingY="5px">
-          <HStack>
-            <Image
-              boxSize={"32px"}
-              borderRadius={8}
-              src={getCroppedUrlImage(genres.image_background)}
-            />
-            <Text fontSize="lg">{genres.name}</Text>
-          </HStack>
-        </ListItem>
+        <Skeleton isLoaded={!isLoading} key={genres.id}>
+          <ListItem key={genres.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize={"32px"}
+                borderRadius={8}
+                src={getCroppedUrlImage(genres.image_background)}
+              />
+              <Text fontSize="lg">{genres.name}</Text>
+            </HStack>
+          </ListItem>
+        </Skeleton>
       ))}
     </List>
   );
