120d501e30536f5880277abbe8289630d991ac0c
nitin-pandita <nitin_207013@saitm.org>
2023-07-11 19:57:23 +0530

    created Platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Games } from "../hooks/useGame";
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
 interface Props {
   game: Games;
 }
@@ -10,6 +11,10 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
+        {/* <p>Rating - {game.rating} / 5</p> */}
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
